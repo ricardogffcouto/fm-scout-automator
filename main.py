b0a36b7d33dc3d3efecd335c5651ebd808e08c00
Ricardo Couto <ricardo.gff.couto@gmail.com>
2021-08-25 18:27:15 +0100

    Multiplying by weights

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import shutil
 import os
 import pandas as pd
+import numpy as np
 
 INITIAL_FILTER_Y = 807
 FILTER_OFFSET_Y = 30
 NUM_FILTERS = 4
+
+
+def dict_weight_parser(w_dict):
+    weighted_dict = {}
+
+    for k in w_dict.keys():
+        for p in k.split(', '):
+            weighted_dict[p] = w_dict[k]
+    
+    return weighted_dict
 
 def activate_fm():
     window_id = os.popen("xdotool search --name 'Football Manager 2019 Touch'").read()
@@ -12,12 +23,15 @@ def activate_fm():
     os.system('xdotool windowactivate %s' % (window_id) )
     os.system("xdotool sleep 0.5")
 
+
 def sleep():
     os.system("xdotool sleep 0.5")
+
 
 def move_click(x, y):
     os.system("xdotool mousemove %s %s click 1" % (x, y))
     sleep()
+
 
 def extract_scouting(filter_id):
     # Select filter
@@ -41,13 +55,14 @@ def extract_scouting(filter_id):
     dst_path = './scouting1.html'
     shutil.move(src_path, dst_path)
 
+
 def scout():
     activate_fm()
     for filter_id in range(NUM_FILTERS):
         extract_scouting(filter_id)
 
 
-def foot_to_int(foot):
+def foot_to_num(foot):
     foots = {
         'Very Strong': 1,
         'Strong': 0.8,
@@ -58,31 +73,81 @@ def foot_to_int(foot):
     }
     return foots[foot]
 
-def position_to_str(pos):
-    player_pos = []
-    positions = pos.split(', ')
 
-    for p in positions:
-        p = p.split(' ')
-        posits = p[0].split('/')
+def load_shortlist():
+    def position_to_str(pos):
+        player_pos = []
+        positions = pos.split(', ')
 
-        sides = ['C']        
-        if len(p) > 1:
-            sides = list(p[1].replace('(','').replace(')',''))
+        for p in positions:
+            p = p.split(' ')
+            posits = p[0].split('/')
 
-        for posit in posits:
-            for side in sides:
-                player_pos.append('%s (%s)' % (posit, side))
+            sides = ['C']        
+            if len(p) > 1:
+                sides = list(p[1].replace('(','').replace(')',''))
 
-    return ', '.join(player_pos)
+            for posit in posits:
+                for side in sides:
+                    player_pos.append('%s (%s)' % (posit, side))
+
+        return ', '.join(player_pos)
+
+    PERSONALITY_WEIGHTS = {
+        'Model Citizen, Resolute, Model Professional, Driven, Iron Willed': 1.05,
+        'Perfectionist, Professional, Fairly Professional, Spirited, Very Ambitious, Ambitious, Determined, Fairly Determined, Charismatic Leader, Born Leader, Leader, Resilient': 1.01,
+        'Jovial, Light Hearted, Devoted / Very Loyal, Loyal, Fairly Loyal, Honest, Sporting, Fairly Sporting, Realist, Balanced, Fairly Ambitious, Light-Hearted': 1,
+        'Unsporting, Slack, Casual, Temperamental, Unambitious, Easily Discouraged, Low Determination, Spineless, Low Self Belief, Mercenary, Fickle': 0.7,
+    }
+
+    PERSONALITIES = dict_weight_parser(PERSONALITY_WEIGHTS)
 
 
-def df_apply_column(column, function):
-    scouting_list[column] = scouting_list[column].apply(function)
+    def personality_to_num(per):
+        return PERSONALITIES[per]
 
-scouting_list = pd.read_html('scouting1.html', header=0)[0]
-df_apply_column('Left Foot', foot_to_int)
-df_apply_column('Right Foot', foot_to_int)
-df_apply_column('Position', position_to_str)
-df_apply_column('Sec. Position', position_to_str)
-print(scouting_list)
+
+    def shortlist_apply_column(column, function):
+        SHORTLIST[column] = SHORTLIST[column].apply(function)
+
+
+    SHORTLIST = pd.read_html('scouting1.html', header=0)[0]
+
+    # Transform data
+
+    shortlist_apply_column('Left Foot', foot_to_num)
+    shortlist_apply_column('Right Foot', foot_to_num)
+    shortlist_apply_column('Position', position_to_str)
+    shortlist_apply_column('Sec. Position', position_to_str)
+    shortlist_apply_column('Personality', personality_to_num)
+
+    # Adding scouting data
+
+    SHORTLIST['Footedness'] = SHORTLIST['Right Foot'] - SHORTLIST['Left Foot']
+    SHORTLIST['Youth Factor'] = np.minimum(np.maximum(1 - (SHORTLIST['Age'] - 18) / 20, 0), 1)
+
+    return SHORTLIST
+
+
+def is_position(positions, pos):
+    posits = positions.str.split(pat=", ")
+    for p in posits:
+        if p[0].split(' ')[0] == pos:
+            return True
+    return False
+
+
+def scouting_report(shortlist, positions = [], youth_importance = 0.5):
+    pos_weights = pd.read_excel('PosWeights.ods', engine='odf')
+
+    weights = pos_weights.loc[lambda df: (df['Player Position'] == 'D') & (df['Side'] == 'L'), :]
+
+    shortlist['Main Pos.'] = shortlist['Position'].apply(lambda pos: pos.split(', ')[0].split(' ')[0] + pos.split(', ')[0].split(' ')[1].replace('(', '').replace(')', ''))
+
+    attributes = shortlist.columns.intersection(weights.columns)
+
+    shortlist[attributes] = shortlist[attributes].mul(np.array(weights[attributes]), axis='columns', fill_value=0)
+
+    return shortlist
+
+print(scouting_report(load_shortlist(), positions=['DL']))
